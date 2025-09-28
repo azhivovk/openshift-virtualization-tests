79fa30984c94c261f8b42f8ae55c1544cbbabf13
Asia Khromov <azhivovk@redhat.com>
2025-09-28 15:15:07 +0300

    net, localnet, jumbo frame: Add jumbo frame no fragmentation test
    Add new test: Verify tcp connectivity using localnet and jumbo frames
    over ovs bridge, without fragmentaion
    This test combines the existing localnet and jumbo frame tests
    The test includes 2 steps:
      1) Verify jumbo frames are not fragmented with ping
      2) Verify connectivity using iperf3 client-server with jumbo frame
    
    Ping runs with a console command without using ssh because it is
    possible to run assert_ping_successful (due to use of ssh_exec) only
    with instance of VirtualMachineForTest VMs, while the VMs which are used for
    iperf3 client-server are instance of BaseVirtualMachine
    
    Signed-off-by: Asia Khromov <azhivovk@redhat.com>

--- tests/network/localnet/test_ovs_bridge.py
+++ tests/network/localnet/test_ovs_bridge.py
@@ -7,7 +7,8 @@ from tests.network.localnet.liblocalnet import (
     LOCALNET_OVS_BRIDGE_NETWORK,
     client_server_active_connection,
 )
-from utilities.virt import migrate_vm_and_verify
+from utilities.network import get_vmi_ip_v4_by_name
+from utilities.virt import migrate_vm_and_verify, vm_console_run_commands
 
 
 @pytest.mark.ipv4
@@ -44,3 +45,25 @@ def test_connectivity_after_interface_state_change_in_ovs_bridge_localnet_vms(
         port=8888,
     ) as (client, server):
         assert is_tcp_connection(server=server, client=client)
+
+
+@pytest.mark.polarion("CNV-12349")
+@pytest.mark.usefixtures("nncp_localnet_on_secondary_node_nic_config_mtu")
+def test_tcp_connectivity_ovs_bridge_jumbo_frames_no_fragmentation(
+    cudn_localnet_ovs_bridge_jumbo_frame,
+    ovs_bridge_localnet_running_jumbo_frame_vms,
+    localnet_ovs_bridge_jumbo_frame_server,
+    localnet_ovs_bridge_jumbo_frame_client,
+    cluster_hardware_mtu,
+):
+    icmp_header = 8
+    ip_header = 20
+    ping_packet_size = cluster_hardware_mtu - icmp_header - ip_header
+    vm1, vm2 = ovs_bridge_localnet_running_jumbo_frame_vms
+    dst_ip = get_vmi_ip_v4_by_name(vm=vm2, name=cudn_localnet_ovs_bridge_jumbo_frame.name)
+    ping_cmd_jumbo_frame_no_fragmentation = f"ping -q -c 3 {dst_ip} -s {ping_packet_size} -M do"
+    vm_console_run_commands(vm=vm1, commands=[ping_cmd_jumbo_frame_no_fragmentation])
+
+    assert is_tcp_connection(
+        server=localnet_ovs_bridge_jumbo_frame_server, client=localnet_ovs_bridge_jumbo_frame_client
+    )
