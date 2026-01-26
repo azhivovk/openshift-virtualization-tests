2622bfb746dd660de0f637c46318594349a6f67d
Asia Khromov <azhivovk@redhat.com>
2026-01-26 14:21:48 +0200

    net, sriov: Adjust tests to IPv6
    SR-IOV net tests are missing IPv6 masquerade address
    to enable ssh to guest VM on IPv6 single-stack cluster,
    in addition to a missing IPv6 address on the secondary
    interface.
    
    On this change, all VMs used in tests are assigned with
    IPv6 address on the primary interface and a randomly
    generated IPV6 address on the secondary interface.
    
    Signed-off-by: Asia Khromov <azhivovk@redhat.com>

--- tests/network/sriov/test_sriov.py
+++ tests/network/sriov/test_sriov.py
@@ -17,37 +17,43 @@ LOGGER = logging.getLogger(__name__)
 pytestmark = [pytest.mark.special_infra, pytest.mark.sriov]
 
 
+@pytest.mark.parametrize(
+    "ip_family",
+    [
+        pytest.param(4, marks=pytest.mark.ipv4),
+        pytest.param(6, marks=pytest.mark.ipv6),
+    ],
+)
 class TestPingConnectivity:
     @pytest.mark.post_upgrade
-    @pytest.mark.ipv4
     @pytest.mark.polarion("CNV-3963")
     def test_sriov_basic_connectivity(
         self,
+        ip_family,
         sriov_network,
         sriov_vm1,
         sriov_vm2,
     ):
         assert_ping_successful(
             src_vm=sriov_vm1,
-            dst_ip=lookup_iface_status_ip(vm=sriov_vm2, iface_name=sriov_network.name, ip_family=4),
+            dst_ip=lookup_iface_status_ip(vm=sriov_vm2, iface_name=sriov_network.name, ip_family=ip_family),
         )
 
-    @pytest.mark.ipv4
     @pytest.mark.polarion("CNV-4505")
+    @pytest.mark.usefixtures("sriov_network_mtu_9000")
     def test_sriov_custom_mtu_connectivity(
         self,
+        ip_family,
         sriov_network,
         sriov_vm1,
         sriov_vm2,
-        sriov_network_mtu_9000,
     ):
         assert_ping_successful(
             src_vm=sriov_vm1,
-            dst_ip=lookup_iface_status_ip(vm=sriov_vm2, iface_name=sriov_network.name, ip_family=4),
+            dst_ip=lookup_iface_status_ip(vm=sriov_vm2, iface_name=sriov_network.name, ip_family=ip_family),
             packet_size=MTU_9000,
         )
 
-    @pytest.mark.ipv4
     @pytest.mark.polarion("CNV-3958")
     @pytest.mark.xfail(
         reason=f"{QUARANTINED}: fails in CI due to issue in specific cluster; tracked in CNV-75730",
@@ -55,16 +61,16 @@ class TestPingConnectivity:
     )
     def test_sriov_basic_connectivity_vlan(
         self,
+        ip_family,
         sriov_network_vlan,
         sriov_vm3,
         sriov_vm4,
     ):
         assert_ping_successful(
             src_vm=sriov_vm3,
-            dst_ip=lookup_iface_status_ip(vm=sriov_vm4, iface_name=sriov_network_vlan.name, ip_family=4),
+            dst_ip=lookup_iface_status_ip(vm=sriov_vm4, iface_name=sriov_network_vlan.name, ip_family=ip_family),
         )
 
-    @pytest.mark.ipv4
     @pytest.mark.polarion("CNV-4713")
     @pytest.mark.xfail(
         reason=f"{QUARANTINED}: fails in CI due to issue in specific cluster; tracked in CNV-75730",
@@ -72,20 +78,22 @@ class TestPingConnectivity:
     )
     def test_sriov_no_connectivity_no_vlan_to_vlan(
         self,
+        ip_family,
         sriov_network_vlan,
         sriov_vm1,
         sriov_vm4,
     ):
         assert_no_ping(
             src_vm=sriov_vm1,
-            dst_ip=lookup_iface_status_ip(vm=sriov_vm4, iface_name=sriov_network_vlan.name, ip_family=4),
+            dst_ip=lookup_iface_status_ip(vm=sriov_vm4, iface_name=sriov_network_vlan.name, ip_family=ip_family),
         )
 
+
+class TestSriovInterfacePersistence:
     @pytest.mark.post_upgrade
     @pytest.mark.polarion("CNV-4768")
     def test_sriov_interfaces_post_reboot(
         self,
-        sriov_vm4,
         vm4_interfaces,
         restarted_sriov_vm4,
     ):
@@ -93,12 +101,18 @@ class TestPingConnectivity:
         assert restarted_sriov_vm4.vmi.interfaces[1] == vm4_interfaces[1]
 
 
-@pytest.mark.special_infra
+@pytest.mark.parametrize(
+    "ip_family",
+    [
+        pytest.param(4, marks=pytest.mark.ipv4),
+        pytest.param(6, marks=pytest.mark.ipv6),
+    ],
+)
 class TestSriovLiveMigration:
-    @pytest.mark.ipv4
     @pytest.mark.polarion("CNV-6455")
     def test_sriov_migration(
         self,
+        ip_family,
         sriov_network,
         sriov_vm_migrate,
         sriov_vm2,
@@ -106,7 +120,7 @@ class TestSriovLiveMigration:
         migrate_vm_and_verify(vm=sriov_vm_migrate, check_ssh_connectivity=True)
         assert_ping_successful(
             src_vm=sriov_vm2,
-            dst_ip=lookup_iface_status_ip(vm=sriov_vm_migrate, iface_name=sriov_network.name, ip_family=4),
+            dst_ip=lookup_iface_status_ip(vm=sriov_vm_migrate, iface_name=sriov_network.name, ip_family=ip_family),
         )
 
 
