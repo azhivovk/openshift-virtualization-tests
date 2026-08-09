3d9f87ee66fa01425898fccf399b5ad219a401c5
Ohad Revah <orevah@redhat.com>
2026-08-09 10:16:45 +0300

    CherryPicked: [cnv-4.19] Remove static inventory tests and dead code (#5914)
    ##### What this PR does / why we need it:
    Remove static inventory tests comparing cluster components against
    hardcoded lists and dead fixtures/unused imports.
    
    Original PR: #5839
    assisted by: claude code claude-opus-4-6
    ##### Which issue(s) this PR fixes:
    
    ##### Special notes for reviewer:
    
    ##### jira-ticket:
    https://redhat.atlassian.net/browse/CNV-94667
    
    Signed-off-by: Ohad <orevah@redhat.com>

--- tests/install_upgrade_operators/strict_reconciliation/test_hco_related_objects.py
+++ tests/install_upgrade_operators/strict_reconciliation/test_hco_related_objects.py
@@ -1,10 +1,8 @@
 import pytest
-from deepdiff import DeepDiff
 
 from tests.install_upgrade_operators.strict_reconciliation.utils import (
     validate_related_objects,
 )
-from utilities.constants import ALL_HCO_RELATED_OBJECTS
 
 pytestmark = [
     pytest.mark.post_upgrade,
@@ -15,20 +13,6 @@ pytestmark = [
 
 
 class TestRelatedObjects:
-    @pytest.mark.polarion("CNV-9843")
-    def test_no_new_hco_related_objects(self, hco_status_related_objects):
-        actual_related_objects = {
-            related_object["name"]: related_object["kind"] for related_object in hco_status_related_objects
-        }
-        expected_related_objects = {
-            object_name: object_kind
-            for related_object in ALL_HCO_RELATED_OBJECTS
-            for object_name, object_kind in related_object.items()
-        }
-
-        new_related_objects = DeepDiff(t1=expected_related_objects, t2=actual_related_objects, verbose_level=2)
-        assert not new_related_objects, f"There are new HCO related objects:\n {new_related_objects.to_json(indent=2)}"
-
     @pytest.mark.polarion("CNV-7267")
     def test_hco_related_objects(
         self,
